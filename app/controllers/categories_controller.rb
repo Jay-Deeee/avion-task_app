9e62994a70d72bc6dfc4c0f79cde4dd5e8b28c49
Jay-Deeee <144668463+Jay-Deeee@users.noreply.github.com>
2025-04-09 16:30:32 +0800

    Added CRUD to category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,29 +1,62 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
+  # rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  # rescue_from ActiveRecord::InvalidForeignKey, with: :invalid_foreign_key
+
   def index
     @categories = Category.all
     @today = Category.find_by(date: Date.today)
   end
 
   def show
-    @category = Category.find(params[:id])
+    # @task = @category.tasks.new
   end
 
-  def new; end
+  def new
+    @category = Category.new
+  end
 
-  def create; end
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to @category, notice: "Category: '#{@category.title}' saved successfully."
+    else
+      flash[:alert] = "Failed to save category."
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   def edit; end
 
-  def update; end
+  def update
+    if @category.update(category_params)
+      redirect_to @category, notice: "Category has been updated."
+    else
+      flash[:alert] = "Failed to update category."
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
-  def destroy; end
+  def destroy
+    @category.destroy
+    redirect_to root_path, status: :see_other, notice: "Category has been deleted."
+  end
 
   private
-  def set_article
+  def set_category
     @category = Category.find(params[:id])
   end
 
   def category_params
     params.require(:category).permit(:title, :date, :status)
   end
+
+  # def record_not_found
+  #   redirect_to articles_path, alert: "Record does not exist."
+  # end
+
+  # def invalid_foreign_key
+  #   redirect_to articles_path, alert: "Unable to delete category. Still referenced from tasks."
+  # end
 end
